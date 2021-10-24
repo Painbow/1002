8cf7cd547eb8d2683f43066d7ce8740dcd5161f5
Painbow <92162946+Painbow@users.noreply.github.com>
2021-10-24 23:12:43 +0800

    Delete graphFile.py

--- graphFile.py
+++
@@ -1,5 +0,0 @@
-import Functions
-
-timeStamps = Functions.getPastDays(5)
-availTime = Functions.getCarparkAvailAtTime(timeStamps,"HE12")
-Functions.plotGraph("Carpark Availability for the Past 5 days","Time","Carpark Avail",timeStamps,availTime)
