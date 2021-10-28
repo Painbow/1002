0facd66ab6155ee712b1a4609c921737550ab9de
Painbow <92162946+Painbow@users.noreply.github.com>
2021-10-29 00:05:53 +0800

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -7,19 +7,22 @@ from pyproj import Transformer
 from datetime import datetime , timedelta
 import matplotlib.pyplot as plt
 
-resource_id = "139a3035-e624-4f56-b63f-89ae28d4ae4c"
+
 carparkAvailUrl = "https://api.data.gov.sg/v1/transport/carpark-availability?"
-carparkInfoUrl = "https://data.gov.sg/api/action/datastore_search?resource_id="
+
+# Obtains JSON output from a link
 def getOutput(x):
     r = requests.get(x)
     return json.loads(r.content)
 
+# Obtain JSON output containing carpark availability entries
 def getAllCarparkAvail():
     carparkAvailJson = getOutput(carparkAvailUrl)
     carpark_data = carparkAvailJson['items']
     listOfCarparkData = carpark_data[0]['carpark_data']
     return listOfCarparkData
 
+# Get Carpark Availability entries, corresponding to the filtered Carpark Information
 def getCarparkAvail(listOfCarparkData,carparkNo):
     carparkInfo = None
     for carpark in listOfCarparkData:
@@ -28,24 +31,13 @@ def getCarparkAvail(listOfCarparkData,carparkNo):
             carparkInfo = carpark['carpark_info']
     return carparkInfo
 
-def getCurrentTime():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    return current_time
-
+# Converts the SYV1 coordinates from the table into longitude and latitude for a Google Map result
 def openlink(y1, x1):
-
-    # yCoord, xCoord = 103.8846673,1.3213042
     xfm = pyproj.Transformer.from_crs('EPSG:3414', 'EPSG:4326', always_xy=True)
     yCoord, xCoord = xfm.transform(x1, y1)
     webbrowser.open('https://www.google.com/maps/search/' + str(xCoord) + ',' + str(yCoord))
 
-# Converts the x & y coordinates to longitude & latitude to be used for google map search
-def convertXYToLatLong(y1, x1):
-    transformer = Transformer.from_crs('epsg:3414', 'epsg:4326', always_xy=True)
-    return transformer.transform(y1, x1)
-
-
+# Obtains list of timestamps to use for obtaining past data
 def getPastDays(totalDays):
     days = []
     now = datetime.now()
@@ -57,21 +49,23 @@ def getPastDays(totalDays):
         days.append(newDate)
     return days
 
+# Gets past carpark availability information
 def getCarparkAvailAtTime(listOfTime,carparkNo):
     availList = []
     for date in listOfTime:
-        availLots = 0
+        avail_lots = 0
         url = carparkAvailUrl + "date_time=" + date
         result = getOutput(url)
         carpark_data = result['items']
         listOfCarparkData = carpark_data[0]['carpark_data']
         for carparks in listOfCarparkData:
             if carparks["carpark_number"] == carparkNo:
-                availLots = int(carparks["carpark_info"][0]["lots_available"])
-                availList.append(availLots)
+                avail_lots = int(carparks["carpark_info"][0]["lots_available"])
+                availList.append(avail_lots)
 
     return availList
 
+# Plots graph using past carpark availability information
 def plotGraph(title,xlabel,ylabel,xvalue,yvalue):
     plt.plot(xvalue, yvalue)
     plt.xlabel(xlabel)
